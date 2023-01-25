01e544fa40b8cdf4dab87a95aabc1ca5e7eb4e20
Kolade Adelaiye <adelaiyekolade@yahoo.com>
2023-01-25 21:32:47 +0000

    add h2 lines

--- scripts/myFirstButton.js
+++ scripts/myFirstButton.js
@@ -1,7 +1,7 @@
 console.log('JavaScript file is linked correctly');
 
 const myFirstButton = () => {
-    const buttonContent = ['bag', 'purse', 'shoes', 'computers', 'lessons'];
+    const buttonContent = ['bag', 'purse', 'shoes', 'computers', 'lessons', 'kola'];
     let randomButtonContent = Math.floor(Math.random() * buttonContent.length);
     console.log(buttonContent[randomButtonContent]);
     document.getElementById('content').innerText = buttonContent[randomButtonContent];
