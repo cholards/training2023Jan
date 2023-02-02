345bf7c028a91da9c291cec87bdc8e67a15e73cf
Kolade Adelaiye <adelaiyekolade@yahoo.com>
2023-02-02 11:12:20 +0000

    add objects

--- scripts/myFirstButton.js
+++ scripts/myFirstButton.js
@@ -28,3 +28,57 @@ const pageItemList = () => {
 // let adelaiyeFamily = ['iyabo', 'tunne', 'abey', 'nike', 'yemi', 'kola'];
 // const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24, 999]
 // let alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+
+const person = {
+    firstName: 'Asabeneh',
+    lastName: 'Yetayeh',
+    age: 250,
+    country: 'Finland',
+    city: 'Helsinki',
+    skills: [
+        'HTML',
+        'CSS',
+        'JavaScript',
+        'React',
+        'Node',
+        'MongoDB',
+        'Python',
+        'D3.js'
+    ],
+    getFullName: function() {
+        return `${this.firstName} ${this.lastName}`
+    },
+    FunString: function() {
+        return `${this.city} is the coolest place to be`
+    },
+    'phone number': '+3584545454545'
+}
+
+person.location = 'Lagos';
+// person.skills.push('Multimedia, Public Relations, Logistics');
+
+person.setSring = function() {
+
+    let withOutLast = this.skills.pop(-1)
+
+    return `${this.firstName} is the one that teaches us ${this.skills} and ${withOutLast} `
+}
+
+// accessing values using .
+console.log(person.firstName)
+console.log(person.lastName)
+console.log(person.age)
+console.log(person.location) // undefined
+
+// value can be accessed using square bracket and key name
+console.log(person['firstName'])
+console.log(person['lastName'])
+console.log(person['age'])
+console.log(person['age'])
+console.log(person['location']) // undefined
+
+// for instance to access the phone number we only use the square bracket method
+console.log(person['phone number'])
+console.log(person.FunString());
+console.log(person.setSring());
