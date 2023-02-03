1f72d1974ee6078133a778055d8d2e2f28b2cb1a
Kolade Adelaiye <adelaiyekolade@yahoo.com>
2023-02-03 18:19:38 +0000

    Time interval

--- scripts/myFirstButton.js
+++ scripts/myFirstButton.js
@@ -30,180 +30,200 @@ const pageItemList = () => {
 // let alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 
-const person = {
-    firstName: 'Asabeneh',
-    lastName: 'Yetayeh',
-    age: 250,
-    country: 'Finland',
-    city: 'Helsinki',
-    skills: [
-        'HTML',
-        'CSS',
-        'JavaScript',
-        'React',
-        'Node',
-        'MongoDB',
-        'Python',
-        'D3.js'
-    ],
-    getFullName: function() {
-        return `${this.firstName} ${this.lastName}`
-    },
-    FunString: function() {
-        return `${this.city} is the coolest place to be`
-    },
-    'phone number': '+3584545454545'
-}
+// const person = {
+//     firstName: 'Asabeneh',
+//     lastName: 'Yetayeh',
+//     age: 250,
+//     country: 'Finland',
+//     city: 'Helsinki',
+//     skills: [
+//         'HTML',
+//         'CSS',
+//         'JavaScript',
+//         'React',
+//         'Node',
+//         'MongoDB',
+//         'Python',
+//         'D3.js'
+//     ],
+//     getFullName: function() {
+//         return `${this.firstName} ${this.lastName}`
+//     },
+//     FunString: function() {
+//         return `${this.city} is the coolest place to be`
+//     },
+//     'phone number': '+3584545454545'
+// }
 
-person.nationality = 'Ethiopian'
-person.country = 'Finland'
-person.title = 'teacher'
-person.skills.push('Meteor')
-person.skills.push('SasS')
-person.isMarried = true
+// person.nationality = 'Ethiopian'
+// person.country = 'Finland'
+// person.title = 'teacher'
+// person.skills.push('Meteor')
+// person.skills.push('SasS')
+// person.isMarried = true
 
-person.getPersonInfo = function() {
-    let withOutLast = this.skills.pop(-1)
-    return `${this.getFullName()} is a ${this.title}.\nHe lives in ${this.country}.\nHe teaches ${this.skills} and ${withOutLast} `
-}
+// person.getPersonInfo = function() {
+//     let withOutLast = this.skills.pop(-1)
+//     return `${this.getFullName()} is a ${this.title}.\nHe lives in ${this.country}.\nHe teaches ${this.skills} and ${withOutLast} `
+// }
 
-// accessing values using .
-console.log(person.firstName)
-console.log(person.lastName)
-console.log(person.age)
-console.log(person.location) // undefined
+// // accessing values using .
+// console.log(person.firstName)
+// console.log(person.lastName)
+// console.log(person.age)
+// console.log(person.location) // undefined
 
-// value can be accessed using square bracket and key name
-console.log(person['firstName'])
-console.log(person['lastName'])
-console.log(person['age'])
-console.log(person['age'])
-console.log(person['location']) // undefined
+// // value can be accessed using square bracket and key name
+// console.log(person['firstName'])
+// console.log(person['lastName'])
+// console.log(person['age'])
+// console.log(person['age'])
+// console.log(person['location']) // undefined
 
-// for instance to access the phone number we only use the square bracket method
-console.log(person['phone number'])
-console.log(person.FunString());
-console.log(person.getPersonInfo());
-
-
-console.log('Object Methods Lessons')
-
-const copyPerson = person.hasOwnProperty('title');
-console.log(copyPerson)
+// // for instance to access the phone number we only use the square bracket method
+// console.log(person['phone number'])
+// console.log(person.FunString());
+// console.log(person.getPersonInfo());
 
 
-const users = {
-    Alex: {
-        email: 'alex@alex.com',
-        skills: ['HTML', 'CSS', 'JavaScript'],
-        age: 20,
-        isLoggedIn: false,
-        points: 30
-    },
-    Asab: {
-        email: 'asab@asab.com',
-        skills: ['HTML', 'CSS', 'JavaScript', 'Redux', 'MongoDB', 'Express', 'React', 'Node'],
-        age: 25,
-        isLoggedIn: false,
-        points: 50
-    },
-    Brook: {
-        email: 'daniel@daniel.com',
-        skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
-        age: 30,
-        isLoggedIn: true,
-        points: 50
-    },
-    Daniel: {
-        email: 'daniel@alex.com',
-        skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
-        age: 20,
-        isLoggedIn: false,
-        points: 40
-    },
-    John: {
-        email: 'john@john.com',
-        skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
-        age: 20,
-        isLoggedIn: true,
-        points: 50
-    },
-    Thomas: {
-        email: 'thomas@thomas.com',
-        skills: ['HTML', 'CSS', 'JavaScript', 'React'],
-        age: 20,
-        isLoggedIn: false,
-        points: 40
-    },
-    Paul: {
-        email: 'paul@paul.com',
-        skills: ['HTML', 'CSS', 'JavaScript', 'MongoDB', 'Express', 'React', 'Node'],
-        age: 20,
-        isLoggedIn: false,
-        points: 40
-    }
-}
+// console.log('Object Methods Lessons')
 
-users.Kola = {
-    email: 'kola@kola.com',
-    skills: ['PR', 'Multimedi', 'JavaScript', 'MongoDB', 'Express', 'React', 'Node', 'Music'],
-    age: 90,
-    isLoggedIn: true,
-    points: 9000
-};
+// const copyPerson = person.hasOwnProperty('title');
+// console.log(copyPerson)
 
-Object.assign(users)
 
-//console.log(users['Alex'])
+// const users = {
+//     Alex: {
+//         email: 'alex@alex.com',
+//         skills: ['HTML', 'CSS', 'JavaScript'],
+//         age: 20,
+//         isLoggedIn: false,
+//         points: 30
+//     },
+//     Asab: {
+//         email: 'asab@asab.com',
+//         skills: ['HTML', 'CSS', 'JavaScript', 'Redux', 'MongoDB', 'Express', 'React', 'Node'],
+//         age: 25,
+//         isLoggedIn: false,
+//         points: 50
+//     },
+//     Brook: {
+//         email: 'daniel@daniel.com',
+//         skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
+//         age: 30,
+//         isLoggedIn: true,
+//         points: 50
+//     },
+//     Daniel: {
+//         email: 'daniel@alex.com',
+//         skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
+//         age: 20,
+//         isLoggedIn: false,
+//         points: 40
+//     },
+//     John: {
+//         email: 'john@john.com',
+//         skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
+//         age: 20,
+//         isLoggedIn: true,
+//         points: 50
+//     },
+//     Thomas: {
+//         email: 'thomas@thomas.com',
+//         skills: ['HTML', 'CSS', 'JavaScript', 'React'],
+//         age: 20,
+//         isLoggedIn: false,
+//         points: 40
+//     },
+//     Paul: {
+//         email: 'paul@paul.com',
+//         skills: ['HTML', 'CSS', 'JavaScript', 'MongoDB', 'Express', 'React', 'Node'],
+//         age: 20,
+//         isLoggedIn: false,
+//         points: 40
+//     }
+// }
 
-let eachPerson = [Object.keys(users)];
-let objectOfScore = {};
-for (eachPerson in users) {
-    objectOfScore[eachPerson] = users[eachPerson].skills.length;
-}
-let namesArray = Object.keys(objectOfScore)
-let skillsArray = Object.values(objectOfScore)
+// users.Kola = {
+//     email: 'kola@kola.com',
+//     skills: ['PR', 'Multimedi', 'JavaScript', 'MongoDB', 'Express', 'React', 'Node', 'Music'],
+//     age: 90,
+//     isLoggedIn: true,
+//     points: 9000
+// };
 
-for (let i = 0; i < namesArray.length; i++) {
-    console.log(`${namesArray[i]} has ${skillsArray[i]} Skills,`)
+// Object.assign(users)
 
-    if (skillsArray[i] === Math.max.apply(null, skillsArray)) {
-        console.log(`${namesArray[i]} has the highest score`)
-    }
-}
+// //console.log(users['Alex'])
 
-const higherOrder = n => {
-    const doSomething = m => {
-        const doWhatEver = t => {
-            return 2 * n + 3 * m + t
-        }
-        return doWhatEver
-    }
-    return doSomething
-}
-console.log(higherOrder(2)(3)(10))
+// let eachPerson = [Object.keys(users)];
+// let objectOfScore = {};
+// for (eachPerson in users) {
+//     objectOfScore[eachPerson] = users[eachPerson].skills.length;
+// }
+// let namesArray = Object.keys(objectOfScore)
+// let skillsArray = Object.values(objectOfScore)
 
-const numbers = [1, 2, 3, 4, 5]
+// for (let i = 0; i < namesArray.length; i++) {
+//     console.log(`${namesArray[i]} has ${skillsArray[i]} Skills,`)
+
+//     if (skillsArray[i] === Math.max.apply(null, skillsArray)) {
+//         console.log(`${namesArray[i]} has the highest score`)
+//     }
+// }
+
+// const higherOrder = n => {
+//     const doSomething = m => {
+//         const doWhatEver = t => {
+//             return 2 * n + 3 * m + t
+//         }
+//         return doWhatEver
+//     }
+//     return doSomething
+// }
+// console.log(higherOrder(2)(3)(10))
+
+// const numbers = [1, 2, 3, 4, 5]
+// const sumArray = arr => {
+//     let sum = 0
+//     const callback = function(element) {
+//         sum += element
+//     }
+//     arr.forEach(callback)
+//     return sum
+
+// }
+// console.log(sumArray(numbers))
+
+
+// // Higher order function returning an other function
+// const higherOrder = n => {
+//     const doSomething = m => {
+//         const doWhatEver = t => {
+//             return 2 * n + 3 * m + t
+//         }
+//         return doWhatEver
+//     }
+//     return doSomething
+// }
+// console.log(higherOrder(2)(3)(10))
+
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+
 const sumArray = arr => {
-    let sum = 0
-    const callback = function(element) {
-        sum += element
+        let sum = 0;
+
+        arr.forEach(x => {
+            sum += x;
+        })
+
+
+        console.log(sum)
+        return sum;
     }
-    arr.forEach(callback)
-    return sum
+    //console.log(sumArray(numbers))
 
-}
-console.log(sumArray(numbers))
+let sayHi = () => console.log('Kola');
 
-
-// Higher order function returning an other function
-const higherOrder = n => {
-    const doSomething = m => {
-        const doWhatEver = t => {
-            return 2 * n + 3 * m + t
-        }
-        return doWhatEver
-    }
-    return doSomething
-}
-console.log(higherOrder(2)(3)(10))
+setInterval(sayHi, 1000);
