4d293f4441e081742f3e8aede6f6694bacb2c3fb
Kolade Adelaiye <adelaiyekolade@yahoo.com>
2023-02-03 12:28:55 +0000

    Refine Function

--- scripts/myFirstButton.js
+++ scripts/myFirstButton.js
@@ -194,3 +194,16 @@ const sumArray = arr => {
 
 }
 console.log(sumArray(numbers))
+
+
+// Higher order function returning an other function
+const higherOrder = n => {
+    const doSomething = m => {
+        const doWhatEver = t => {
+            return 2 * n + 3 * m + t
+        }
+        return doWhatEver
+    }
+    return doSomething
+}
+console.log(higherOrder(2)(3)(10))
