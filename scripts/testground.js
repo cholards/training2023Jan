aa4f00f006ad40c91defc07bc80bdd3c788b2bf1
Kolade Adelaiye <adelaiyekolade@yahoo.com>
2023-02-09 23:02:17 +0000

    Regex

--- scripts/testground.js
+++ scripts/testground.js
@@ -3,22 +3,28 @@ export function kold(n) {
     for (let i = 1; i <= n; i++) {
 
 
-        if (i % 15 === 0) {
-            // console.log('fizzbuzz')
-            return 'fizzbuzz'
-        } else if (i % 3 === 0) {
-            // console.log('fizz')
-            return 'fizz';
-        } else if (i % 2 === 0) {
-            // console.log('buzz')
-            return 'buzz'
-        } else {
-            //  console.log(i)
-            return i
-        }
+        // if (i % 3 == 0 && i % 2 == 0) {
+        //     console.log('fizzbuzz')
+        //         // return 'fizzbuzz'
+        // } else if (i % 3 == 0) {
+        //     console.log('fizz')
+        //         // return 'fizz';
+        // } else if (i % 2 == 0) {
+        //     console.log('buzz')
+        //         // return 'buzz'
+        // } else {
+        //     let result = i;
+        //     console.log(result)
+        //         //return i
+        // }
 
     }
 
 }
 
-kold(37);
+console.log(kold(37));
+
+const str = 'I love JavaScript'
+const pattern = /love/
+const result = pattern.test(str)
+console.log(result)
--- scripts/testground.test.js
+++ scripts/testground.test.js
@@ -5,8 +5,5 @@ describe('kola', () => {
     it('logs fizzbuzz if result is divisible by 15', () => {
         expect(kold(15)).toBe('fizzbuzz')
     });
-    it('logs fizz if result is divisible by 3', () => {
-        expect(kold(3)).toBe('fizz')
-    });
 
 });
