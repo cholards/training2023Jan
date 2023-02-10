50e224e6e5f7a139f9e1471378f0f4247512f5ea
Kolade Adelaiye <adelaiyekolade@yahoo.com>
2023-02-10 22:16:43 +0000

    Updates

--- scripts/testground.js
+++ scripts/testground.js
@@ -3,20 +3,20 @@ export function kold(n) {
     for (let i = 1; i <= n; i++) {
 
 
-        // if (i % 3 == 0 && i % 2 == 0) {
-        //     console.log('fizzbuzz')
-        //         // return 'fizzbuzz'
-        // } else if (i % 3 == 0) {
-        //     console.log('fizz')
-        //         // return 'fizz';
-        // } else if (i % 2 == 0) {
-        //     console.log('buzz')
-        //         // return 'buzz'
-        // } else {
-        //     let result = i;
-        //     console.log(result)
-        //         //return i
-        // }
+        if (i % 3 == 0 && i % 2 == 0) {
+            console.log('fizzbuzz')
+                // return 'fizzbuzz'
+        } else if (i % 3 == 0) {
+            console.log('fizz')
+                // return 'fizz';
+        } else if (i % 2 == 0) {
+            console.log('buzz')
+                // return 'buzz'
+        } else {
+            let result = i;
+            console.log(result)
+                //return i
+        }
 
     }
 
