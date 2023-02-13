3f5e020d24054e60f694f3fc69e3711aa491e010
Kolade Adelaiye <adelaiyekolade@yahoo.com>
2023-02-13 23:15:46 +0000

    Maps

--- scripts/partTwo.js
+++ scripts/partTwo.js
@@ -1,31 +1,31 @@
-export function sum(a, b) {
-    return a + b;
-}
+// export function sum(a, b) {
+//     return a + b;
+// }
 
 
-export function fizz_buzz(num) {
-    if (num % 15 === 0) {
-        return 'fizzbuzz';
-    } else if (num % 3 === 0) {
-        return 'fizz';
-    } else if (num % 5 === 0) {
-        return 'buzz';
-    } else {
-        return ' ';
-    }
-}
+// export function fizz_buzz(num) {
+//     if (num % 15 === 0) {
+//         return 'fizzbuzz';
+//     } else if (num % 3 === 0) {
+//         return 'fizz';
+//     } else if (num % 5 === 0) {
+//         return 'buzz';
+//     } else {
+//         return ' ';
+//     }
+// }
 
-export function fizz_buzz_total() {
-    let result = [];
-    for (var i = 0; i < 101; i++) {
-        result.push(fizz_buzz(i));
-        console.log(i + ' is: ' + fizz_buzz(i));
-        // document.getElementById('fizz_buzz').innerText = result;
-    }
-    return result;
-}
+// export function fizz_buzz_total() {
+//     let result = [];
+//     for (var i = 0; i < 101; i++) {
+//         result.push(fizz_buzz(i));
+//         console.log(i + ' is: ' + fizz_buzz(i));
+//         // document.getElementById('fizz_buzz').innerText = result;
+//     }
+//     return result;
+// }
 
 
-export function kola(a, b) {
-    return a + b;
-}
+// export function kola(a, b) {
+//     return a + b;
+// }
--- scripts/testground.js
+++ scripts/testground.js
@@ -1,30 +1,59 @@
-export function kold(n) {
+// function counctDown(x) {
+//     if (x === 0) {
+//         return
+//     }
+//     console.log(x)
+//     counctDown(x - 1)
+// }
+// counctDown(20);
 
-    for (let i = 1; i <= n; i++) {
+// function factorial(num) {
+//     // The factorial of 5 = 5*4*3*2*1 
+//     if (num === 0) {
+//         return 1;
+//     } else {
+//         return num * factorial(num - 1);
+//     }
 
+// }
 
-        if (i % 3 == 0 && i % 2 == 0) {
-            console.log('fizzbuzz')
-                // return 'fizzbuzz'
-        } else if (i % 3 == 0) {
-            console.log('fizz')
-                // return 'fizz';
-        } else if (i % 2 == 0) {
-            console.log('buzz')
-                // return 'buzz'
-        } else {
-            let result = i;
-            console.log(result)
-                //return i
-        }
+// console.log(factorial(5));
 
-    }
+function fibonacci(num) {
+    // sequence is 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
+    // let num1 = 0
+    // let num2 = 1
+    // let sum
+    // let i = 0
 
+    // for (let i = 0; i < num; i++) {
+    //     sum = num1 + num2;
+    //     num1 = num2
+    //     num2 = sum
+    // }
+    // return num2;
 }
 
-console.log(kold(37));
+// Given a string str and two characters X and Y, the task is to write a recursive function to replace all occurrences of character X with character Y.
+// Input: str = abacd, X = a, Y = x 
+// Output: xbxcd
 
-const str = 'I love JavaScript'
-const pattern = /love/
-const result = pattern.test(str)
-console.log(result)
+// Input: str = abacd, X = e, Y = y 
+// Output: abacd
+
+function replaceChar(str, c1, c2) {
+    if (str.length === 1) return str;
+    let frontChar = str.charAt(0);
+    if (str.charAt(0) === c1) {
+        frontChar = c2;
+        str = c2 + str.substring(1);
+    }
+    return frontChar + replaceChar(str.substring(1), c1, c2)
+}
+
+
+let word = "koladeadelaiye/Desktop/Nneka/scripts/testground"
+let X = "a"
+let Y = "x"
+
+console.log(replaceChar(word, X, Y))
