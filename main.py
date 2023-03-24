95fc46c62d35262a0179f6c2a036108c449e6742
Kolade Adelaiye <adelaiyekolade@yahoo.com>
2023-03-24 11:43:45 +0000

    Improve on .py Script

--- main.py
+++ main.py
@@ -3,3 +3,27 @@
 user_input = input("enter your name: ")
 
 print("Hello " + user_input)Kolade
+
+
+# This application takes in your name, age, house number and street name
+name = input("Enter your name: ") 
+age = int(input("Enter your age: ")) # Casting to integer to ensure input is a number
+house_number = int(input("Enter your house number: ")) # Casting to integer to ensure input is a number
+street_name = input("Enter your street name: ")
+
+# The code below prints out details collected to the screen. 
+# The information concatenates some strings with the inputs and the nubers in
+# the inputs are display after casting them back to string
+print('This is ' + name + '. He is ' + str(age) + 'years old and lives at house number ' + str(house_number) + ' on ' + street_name + ' Street')
+
+
+
+'''This programe takes in two inputs. First the name of the user, and second
+the age of the user. I prints out the name and age and then prints out the string"Hello World!"'''
+
+user_name = input("Input your name: ")
+print(user_name)
+user_age = int(input("Input you age: "))
+print(user_age)
+
+print("Hello World!")
