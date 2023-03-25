8a68fd8557d27ac662474b3badcdbf449a5f3776
Kolade Adelaiye <adelaiyekolade@yahoo.com>
2023-03-25 12:42:09 +0000

    .py string functions

--- main.py
+++ main.py
@@ -1,29 +1,12 @@
-# Create a variable to store the variable to store the name input
-"""" This is a comment """
-user_input = input("enter your name: ")
+# Type a long string and change every other charcter to capital
 
-print("Hello " + user_input)Kolade
+alphabets = "abcdefghijklmnopqrstuvwxyz"
 
+# print(alphabets[0::2].upper())
+alphabets_lenth = len(alphabets)-1
 
-# This application takes in your name, age, house number and street name
-name = input("Enter your name: ") 
-age = int(input("Enter your age: ")) # Casting to integer to ensure input is a number
-house_number = int(input("Enter your house number: ")) # Casting to integer to ensure input is a number
-street_name = input("Enter your street name: ")
-
-# The code below prints out details collected to the screen. 
-# The information concatenates some strings with the inputs and the nubers in
-# the inputs are display after casting them back to string
-print('This is ' + name + '. He is ' + str(age) + 'years old and lives at house number ' + str(house_number) + ' on ' + street_name + ' Street')
-
-
-
-'''This programe takes in two inputs. First the name of the user, and second
-the age of the user. I prints out the name and age and then prints out the string"Hello World!"'''
-
-user_name = input("Input your name: ")
-print(user_name)
-user_age = int(input("Input you age: "))
-print(user_age)
-
-print("Hello World!")
+for i in range(0, len(alphabets)):
+    if i%2 == 0:
+        print(alphabets[i])
+    else:
+        print(alphabets[i].upper())
