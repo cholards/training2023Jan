ff49f63efc6b2ac3119b7548c3a426a5e7e2365c
Kolade Adelaiye <adelaiyekolade@yahoo.com>
2023-03-26 17:30:04 +0100

    Lists and Dictionaries

--- main.py
+++ main.py
@@ -9,4 +9,10 @@ for i in range(0, len(alphabets)):
     if i%2 == 0:
         print(alphabets[i])
     else:
-        print(alphabets[i].upper())
+       print(alphabets[i].upper())
+        
+my_list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
+double_list = [item * 2 for item in my_list]
+print(double_list)
+
+
