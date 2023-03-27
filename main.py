0d556ed31b8bc44fa8fc1b97a744e42e02ebc189
Kolade Adelaiye <adelaiyekolade@yahoo.com>
2023-03-27 17:13:06 +0100

    Dictionaries

--- main.py
+++ main.py
@@ -5,14 +5,20 @@ alphabets = "abcdefghijklmnopqrstuvwxyz"
 # print(alphabets[0::2].upper())
 alphabets_lenth = len(alphabets)-1
 
-for i in range(0, len(alphabets)):
-    if i%2 == 0:
-        print(alphabets[i])
-    else:
-       print(alphabets[i].upper())
+# for i in range(0, len(alphabets)):
+#     if i%2 == 0:
+#         print(alphabets[i])
+#     else:
+#        print(alphabets[i].upper())
         
-my_list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
-double_list = [item * 2 for item in my_list]
-print(double_list)
+# my_list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
+# double_list = [item * 2 for item in my_list]
+# print(double_list)
 
 
+my_dic = {"name": "Kolade", "surname": "Adelaiye", "age":22, "gender": "male"}
+
+for keys, values in my_dic.items():
+    print(f"{keys.upper()} : {values}")
+    
+print(my_dic.items())
