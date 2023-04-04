8d1c99b3acc551d8187db14e5026d0bca118f621
Kolade Adelaiye <adelaiyekolade@yahoo.com>
2023-04-04 22:57:09 +0100

    Range

--- main.py
+++ main.py
@@ -76,3 +76,14 @@ for key, value in items_tuples:
         pass # To avoid repeating keys.
     else: 
         dict_method_3[key] = value
+        
+        
+        
+        def merge(*args, missing_val = None):
+    #missing_val will be used when one of the smaller lists is shorter tham the others.
+#Get the maximum length within the smaller lists.
+  max_length = max([len(lst) for lst in args])
+  outList = []
+  for i in range(max_length):
+    result.append([args[k][i] if i < len(args[k]) else missing_val for k in range(len(args))])
+  return outList
