f19d1be176e8949c2348e5d9ea2bdc858cd2e46f
Kolade Adelaiye <adelaiyekolade@yahoo.com>
2023-04-25 19:19:53 +0100

    Defensive

--- main.py
+++ main.py
@@ -5,9 +5,15 @@ try: # Exception handling for values that the user enters
         one = float(value_one)
         two = float(value_two)
         
-        # Prompt for user to choose operator
-        operator = input("Enter operator (+, -, x or /) \n \n> ")
-        print(f'{value_one, operator, value_two}')
-        print (one / two)
+        if two != 0:
+            # Prompt for user to choose operator
+            operator = input("Enter operator (+, -, x or /) \n \n> ")
+            print(f'{value_one, operator, value_two}')
+            print (one / two)
+        else:
+            while two == 0 or two != int:
+                value_two = input("Enter second value \n> ")
+                two = float(value_two)
 except Exception:
-        print("Please insert a valid whole or decimal number and begin again")
+        print('''\nYou have entered and invalid input. Please run the program
+again and ensure to insert a valid whole or decimal number \n\n''')
