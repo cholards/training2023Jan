fdff78ec3af7daccefe67a1f7a96f13d886d5cd4
Kolade Adelaiye <adelaiyekolade@yahoo.com>
2023-04-29 09:12:17 +0100

    Tuples

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
+profile_dict = {
+    'name': 'Chris',
+    'surname': 'Smith', 
+    'age': 28, 
+    'cell': '083 233 3242'
+    }
+print (profile_dict['surname']) # prints out 'Smith'
+print (profile_dict.get('cell')) # prints out '083 233 3242'
 
-try: # Exception handling for values that the user enters
-        value_one = input("Enter first value \n> ")
-        value_two = input("Enter second value \n> ")
-        one = float(value_one)
-        two = float(value_two)
-        
-        if two != 0:
-            # Prompt for user to choose operator
-            operator = input("Enter operator (+, -, x or /) \n \n> ")
-            print(f'{value_one, operator, value_two}')
-            print (one / two)
-        else:
-            while two == 0 or two != int:
-                value_two = input("Enter second value \n> ")
-                two = float(value_two)
-except Exception:
-        print('''\nYou have entered and invalid input. Please run the program
-again and ensure to insert a valid whole or decimal number \n\n''')
+
+keys = profile_dict.keys()
+values = profile_dict.values()
+print(keys)
+print(values)
