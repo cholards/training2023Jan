e15f806937de509b681e31c26e2261d4f9916f0b
Kolade Adelaiye <adelaiyekolade@yahoo.com>
2023-05-13 08:37:19 +0100

    Modular Prog

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
-profile_dict = {
-    'name': 'Chris',
-    'surname': 'Smith', 
-    'age': 28, 
-    'cell': '083 233 3242'
-    }
-print (profile_dict['surname']) # prints out 'Smith'
-print (profile_dict.get('cell')) # prints out '083 233 3242'
+def my_function(x):
+    y = x * 2
+    return y
+
+print(my_function(10));
 
 
-keys = profile_dict.keys()
-values = profile_dict.values()
-print(keys)
-print(values)
+def adding(a,b):
+    total = a + b
+    return (description + str(total))
+x = 2
+y = 3
+description = "Total: "
+sum = adding(x, y)
+print(sum)
+
+
+# profile_dict = {
+#     'name': 'Chris',
+#     'surname': 'Smith', 
+#     'age': 28, 
+#     'cell': '083 233 3242'
+#     }
+# print (profile_dict['surname']) # prints out 'Smith'
+# print (profile_dict.get('cell')) # prints out '083 233 3242'
+
+# profile_dict['surname'] = 'Bob'
+# profile_dict['location'] = 'Srilanka'
+
+# # print(profile_dict)
+
+# print('location' in profile_dict)
+
+# # keys = profile_dict.keys()
+# # values = profile_dict.values()
+# # print(keys)
+# # print(values)
