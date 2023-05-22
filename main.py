b63934067a9afdfc3572fdc74e4199754733c514
Kolade Adelaiye <adelaiyekolade@yahoo.com>
2023-05-22 10:57:37 +0100

    ASCI

--- main.py
+++ main.py
@@ -1,76 +1,40 @@
 
-n = int(input("Enter number here: > "))
+# text = input("Enter the word to encrypt")
+# shift = 0
 
-counter = 0
-sum = 0
-
-
-while range(counter, n):
-    counter = counter + 1
-    sum = sum + counter
-
-print(sum)
+# try:
+#     shift = int(input("Enter the shift code"))
+# except Exception as e:
+#     while e:
+#         print("Only an Integer is accepted")
+#         shift = int(input("Enter the shift code"))
 
 
+def encode(input):
+    index = ord(input)
+    shift = 1
+    new_index = index + shift
+
+    # if chr(index) == " ":
+    #     shift = 0
+    #     new_index = index + shift
+    if not chr(index).isalpha():
+        shift = 0
+        new_index = index + shift
+    else:
+        if new_index >= 90 and new_index < 97:
+            new_index = 64 + shift
 
 
+    # if index > 26:
+    #     index = (index % 26)
+    #     new_index = index + shift
+
+    return (f"{input} index is {index}, ------- but transposed to {chr(new_index)} with index {new_index} ")
+    #return chr(new_index)
 
 
-
-
-
-
-# for x in range(0, counter):
-#     sum = x + (x - 1)
-#     counter = counter - 1
-#     print(counter)
-
-
-
-
-
-
-
-
-# # text = input("Enter the word to encrypt")
-# # shift = 0
-
-# # try:
-# #     shift = int(input("Enter the shift code"))
-# # except Exception as e:
-# #     while e:
-# #         print("Only an Integer is accepted")
-# #         shift = int(input("Enter the shift code"))
-    
-# factor = 0
-
-
-# def encode(input):
-#     index = ord(input)
-#     shift = 0
-  
-#     # if chr(index) == " ":
-#     #     shift = 0
-#     #     new_index = index + shift
-#     # elif chr(index).isnumeric():
-#     #     shift = 0
-#     #     new_index = index + shift
-#     # else:
-#     #     if index > 26:
-#     #         index = (index % 26)
-#     #         new_index = index + shift
-    
-    
-#     if index > 26:
-#         index = (index % 26)
-#         new_index = index + shift
-        
-        
-#     return index
-#     #return chr(new_index)
-
-
-# text = 'Kolade is great'
+text = 'Y'
 
 
 # to_turple = text.split(" ")
@@ -81,4 +45,4 @@ print(sum)
 #     encrypted += encode(alph)
     
     
-# print(encrypted)
+print(encode(text))
