e3c14ce95bd03e2eb8a0b504448f24a7305164ce
Kolade Adelaiye <adelaiyekolade@yahoo.com>
2023-05-23 08:35:47 +0100

    ASCII

--- main.py
+++ main.py
@@ -10,39 +10,45 @@
 #         shift = int(input("Enter the shift code"))
 
 
-def encode(input):
-    index = ord(input)
-    shift = 1
-    new_index = index + shift
-
-    # if chr(index) == " ":
-    #     shift = 0
-    #     new_index = index + shift
-    if not chr(index).isalpha():
-        shift = 0
-        new_index = index + shift
-    else:
-        if new_index >= 90 and new_index < 97:
-            new_index = 64 + shift
+# def encode(input):
+#     index = ord(input)
+#  shift = 1
 
 
-    # if index > 26:
-    #     index = (index % 26)
-    #     new_index = index + shift
+#     if not chr(index).isalpha():
+#         shift = 0
+#         new_index = index + shift
+#     else:
+#         if index >= 90 and index < 96:
+#             new_index = index
+#             new_index = index + shift
+#             print(f"MY NEW INDEX IS {new_index}")
+#         elif index >= 122 and index <= 127:
+#             new_index = index - 25
+#         # else:
+#             # print(f"{input} is an Unknown character")
 
-    return (f"{input} index is {index}, ------- but transposed to {chr(new_index)} with index {new_index} ")
-    #return chr(new_index)
+
+#     return (f"{input} index is {index}, ------- but transposed to {chr(new_index)} with index {new_index} ")
+#     # return chr(new_index)
 
 
-text = 'Y'
+# text = 'This is a 3 boo-bob sentence!'
+# text = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+# text = "Z"
+# lis = list(text)
+# encrypted =""
+
+# for l in lis:
+#     encrypted += encode(l)
+
+# print(encrypted)
+# print(encode(text))
+
+upper = {ascii:chr(ascii) for ascii in range(65,91)}
+lower = {ascii:chr(ascii) for ascii in range(97,123)}
+digit = {ascii:chr(ascii) for ascii in range(48,58)}
 
 
-# to_turple = text.split(" ")
-# encrypted = ""
+print (digit)
 
-
-# for alph in text:
-#     encrypted += encode(alph)
-    
-    
-print(encode(text))
